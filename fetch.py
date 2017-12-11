7f30c03112f8cc36435778367c6ca2f7500f00d1
Tomasz Oponowicz <tomasz.oponowicz@zaxo.biz>
2017-12-11 15:43:35 +0100

    Comments about mp3splt and small fixes

--- fetch.py
+++ fetch.py
@@ -29,6 +29,13 @@ FRAG_FILE = "{lang}_{sex}_{url_hash}_{index}.flac"
 FFMPEG_CONVERT = "ffmpeg -i {0} -ss {2} -t {3} -map_metadata -1 {1}"
 FFMPEG_SPLIT = "ffmpeg -i {0} -f segment -segment_time {2} -c copy {1}"
 
+# remove first 30s and limit lenght to 5m
+# -n: remove metadata
+# mp3splt test.mp3 0.30.0 5.30.0 -n -o out
+
+# frag files into 10s chunks
+# mp3splt out.mp3 -t 0.10.0 -o out_@n
+
 JAMS_FILE = "{0}/test.jams".format(TEMP_DIR)
 NOISES_DIR = "noises"
 NOISE_FILES = [
@@ -59,6 +66,7 @@ def fetch_resource(url, path):
 def deform(input):
     jam = jams.JAMS()
 
+    # enable quick loading audio files
     audio = muda.load_jam_audio(jam, input)
 
     input_without_ext = os.path.splitext(input)[0]
@@ -76,10 +84,23 @@ def deform(input):
         muda.save('{0}_speed_{1}.flac'.format(input_without_ext, index), JAMS_FILE, output)
 
     print("Creating noise variations...")
+    # can we speed up loading noises files?
     noise = muda.deformers.BackgroundNoise(n_samples=3, files=NOISE_FILES, weight_min=0.1, weight_max=0.2)
     for index, output in enumerate(noise.transform(audio)):
         muda.save('{0}_noise_{1}.flac'.format(input_without_ext, index), JAMS_FILE, output)
 
+
+# https://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                #f.flush() commented by recommendation from J.F.Sebastian
+    return local_filename
 
 data = pd.read_csv('data.csv')
 
@@ -109,12 +130,15 @@ for sample_index, row in data.iterrows():
     flac_input_path = os.path.join(TEMP_DIR, FLAC_INPUT_FILE)
     frag_pattern = os.path.join(TEMP_DIR, FRAG_FILE.format(lang=language, sex=sex, url_hash=url_hash, index='%d'))
 
-    if os.path.isdir(group):
+    if os.path.isdir(TEMP_DIR):
         shutil.rmtree(TEMP_DIR)
     os.mkdir(TEMP_DIR)
 
     print("Downloading a sample...")
     fetch_resource(url, mp3_input_path)
+
+    # mp3splt test.mp3 0.30.0 5.30.0 -n -o out
+    # mp3splt out.mp3 -t 0.10.0 -o out_@n
 
     print("Converting a sample to FLAC...")
     run_in_foreground(FFMPEG_CONVERT.format(mp3_input_path, flac_input_path, INPUT_OFFSET, input_duration))
@@ -132,8 +156,8 @@ for sample_index, row in data.iterrows():
         # copy frag file to test/valid/test dir
         shutil.copy2(source_path, target_path)
 
-        if not is_test:
-            deform(target_path)
+        # if not is_test:
+        #     deform(target_path)
 
 # remove temporary files
 shutil.rmtree(TEMP_DIR)
